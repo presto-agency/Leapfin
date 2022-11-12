0ce3d0b30f2e0b651833dd9771e8f9f43abcf9d3
Kamarhinanton <Kamarhin_anton@ukr.net>
2022-11-12 10:23:40 +0200

    feat: add some fixes

--- gulp/tasks/svgSprite.js
+++ gulp/tasks/svgSprite.js
@@ -1,7 +1,16 @@
 import spriteSvg from "gulp-svg-sprite"
+import cheerio from "gulp-cheerio";
 
 export const svgSprite = () => {
   return app.gulp.src(`${app.path.src.svgicons}`, {})
+    .pipe(cheerio({
+      run: function ($) {
+        $('[fill]').removeAttr('fill');
+        $('[stroke]').removeAttr('stroke');
+        $('[style]').removeAttr('style');
+      },
+      parserOptions: {xmlMode: true}
+    }))
     .pipe(spriteSvg({
       mode: {
         stack: {
