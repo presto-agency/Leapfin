3f60509221605b4b2c398c0735d6528a110564e1
Kamarhinanton <Kamarhin_anton@ukr.net>
2022-11-12 17:52:10 +0200

    feat: add sub-submenu

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
 import {addClassHeader} from "./modules/functions.js";
 import {callMenu} from "./modules/addSubmenuClass.js";
+import {showSubmenu} from "./modules/showSubmenu.js";
 
 addClassHeader()
 callMenu()
+showSubmenu(document.querySelectorAll('.sub-submenu'))
 
 
