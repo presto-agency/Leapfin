5d97271327ed3ff73fc552c69e9f0b5d36c161ae
Kamarhinanton <Kamarhin_anton@ukr.net>
2022-11-14 17:44:01 +0200

    feat: add hero html

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,11 @@
 import {addClassHeader} from "./modules/functions.js";
 import {callMenu} from "./modules/addSubmenuClass.js";
 import {showSubmenu} from "./modules/showSubmenu.js";
+import {heroAnimation} from "./modules/heroAnimation.js"
 
 addClassHeader()
 callMenu()
 showSubmenu(document.querySelectorAll('.sub-submenu'))
+heroAnimation()
 
 
