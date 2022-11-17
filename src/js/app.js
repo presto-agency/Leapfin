250a6176f35be814a1e7e4823fb9ef34b7de3213
Kamarhinanton <Kamarhin_anton@ukr.net>
2022-11-17 16:05:06 +0200

    fix: remove animation - 2

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,6 @@ import {callMenu} from "./modules/addSubmenuClass.js";
 import {showSubmenu} from "./modules/showSubmenu.js";
 import {heroAnimation} from "./modules/heroAnimation.js"
 import {scrollHeader} from "./modules/scrollHeader.js";
-import {sectionAnimation} from "./modules/sectionAnimation.js";
 
 addClassHeader()
 callMenu()
