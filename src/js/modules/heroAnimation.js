b290de3b31bb7a52b37e9c4c897cbfaaafe9d2b0
Kamarhinanton <Kamarhin_anton@ukr.net>
2022-11-18 11:26:02 +0200

    feat: home optimization - 2

--- src/js/modules/heroAnimation.js
+++ src/js/modules/heroAnimation.js
@@ -1,11 +1,13 @@
 import lottie from "lottie-web";
 
 export const heroAnimation = () => {
-  lottie.loadAnimation({
-    container: document.getElementById('hero'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'files/lottie/lottie-home-s.json'
-  })
+  window.onload = () => {
+    lottie.loadAnimation({
+      container: document.getElementById('hero'),
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: 'files/lottie/lottie-home-s.json'
+    })
+  }
 }
