d87306deab8d6cf3e6ff47ce0697020453ab3528
Kamarhinanton <Kamarhin_anton@ukr.net>
2022-11-21 14:17:58 +0200

    feat: home optimization

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,43 +1,24 @@
 import webp from "gulp-webp";
 import imagemin from "gulp-imagemin";
+import imageminWebp from 'imagemin-webp';
 
 export const images = () => {
   return app.gulp.src(app.path.src.images)
     .pipe(app.plugins.newer(app.path.build.images))
+    .pipe(webp())
+    .pipe(app.gulp.dest(app.path.build.images))
+    .pipe(app.gulp.src(app.path.src.images))
+    .pipe(app.plugins.newer(app.path.build.images))
     .pipe(
-      app.plugins.ifPlugin(
-        app.isBuild,
-        webp()
-      )
-    )
-    .pipe(
-      app.plugins.ifPlugin(
-        app.isBuild,
-        app.gulp.dest(app.path.build.images)
-      )
-    )
-    .pipe(
-      app.plugins.ifPlugin(
-        app.isBuild,
-        app.gulp.src(app.path.src.images)
-      )
-    )
-    .pipe(
-      app.plugins.ifPlugin(
-        app.isBuild,
-        app.plugins.newer(app.path.build.images)
-      )
-    )
-    .pipe(
-      app.plugins.ifPlugin(
-        app.isBuild,
         imagemin({
           progressive: true,
           svgoPlugins: [{removeViewBox: false}],
           interlaced: true,
-          optimizationLevel: 6 //to 7
+          optimizationLevel: 6, //to 7
+          plugins: [
+            imageminWebp({quality: 50})
+          ]
         })
-      )
     )
     .pipe(app.gulp.dest(app.path.build.images))
     .pipe(app.gulp.src(app.path.src.svg))
@@ -45,41 +26,24 @@ export const images = () => {
     .pipe(app.plugins.browsersync.stream())
 }
 
-// export const imagesDesktop = () => {
-//   return app.gulp.src(app.path.src.imagesDesk)
-//     .pipe(app.plugins.newer(app.path.build.imagesDesk))
-//     .pipe(
-//       app.plugins.ifPlugin(
-//         app.isBuild,
-//         webp()
-//       )
-//     )
-//     .pipe(
-//       app.plugins.ifPlugin(
-//         app.isBuild,
-//         app.gulp.dest(app.path.build.imagesDesk)
-//       )
-//     )
-//     .pipe(
-//       app.plugins.ifPlugin(
-//         app.isBuild,
-//         app.gulp.src(app.path.src.imagesDesk)
-//       )
-//     )
-//     .pipe(
-//       app.plugins.ifPlugin(
-//         app.isBuild,
-//         app.plugins.newer(app.path.build.imagesDesk)
-//       )
-//     )
-//     .pipe(
-//         imagemin({
-//           progressive: true,
-//           svgoPlugins: [{removeViewBox: false}],
-//           interlaced: true,
-//           optimizationLevel: 1 //to 7
-//         })
-//     )
-//     .pipe(app.gulp.dest(app.path.build.imagesDesk))
-//     .pipe(app.plugins.browsersync.stream())
-// }
+export const imagesDesktop = () => {
+  return app.gulp.src(app.path.src.imagesDesk)
+    .pipe(app.plugins.newer(app.path.build.imagesDesk))
+    .pipe(webp())
+    .pipe(app.gulp.dest(app.path.build.imagesDesk))
+    .pipe(app.gulp.src(app.path.src.imagesDesk))
+    .pipe(app.plugins.newer(app.path.build.imagesDesk))
+    .pipe(
+        imagemin({
+          progressive: true,
+          svgoPlugins: [{removeViewBox: false}],
+          interlaced: true,
+          optimizationLevel: 2, //to 7
+          plugins: [
+            imageminWebp({quality: 90})
+          ]
+        })
+    )
+    .pipe(app.gulp.dest(app.path.build.imagesDesk))
+    .pipe(app.plugins.browsersync.stream())
+}
