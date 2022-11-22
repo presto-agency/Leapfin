aebded1da0ffca9033c10efa0abc466bad1aeac7
Kamarhinanton <Kamarhin_anton@ukr.net>
2022-11-22 11:20:28 +0200

    feat: add hero block to revenue page

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,6 @@
 import {addClassHeader} from "./modules/addClassToHeader.js";
 import {callMenu} from "./modules/addSubmenuClass.js";
 import {showSubmenu} from "./modules/showSubmenu.js";
-// import {heroAnimation} from "./modules/heroAnimation.js"
 import {scrollHeader} from "./modules/scrollHeader.js";
 
 if(window.innerWidth < 1200){
@@ -9,7 +8,6 @@ if(window.innerWidth < 1200){
   callMenu()
   showSubmenu(document.querySelectorAll('.sub-submenu'))
 }
-// heroAnimation()
 scrollHeader()
 
 
