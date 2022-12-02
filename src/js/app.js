1a57ee3435299494c0b524a885bfa83db5c9489d
Kamarhinanton <Kamarhin_anton@ukr.net>
2022-12-02 12:10:49 +0200

    feat: add api google maps

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ import {callMenu} from "./modules/addSubmenuClass.js";
 import {showSubmenu} from "./modules/showSubmenu.js";
 import {scrollHeader} from "./modules/scrollHeader.js";
 import {callTabs} from "./modules/callTabs.js";
+import {googleMap} from "./modules/googleMap.js";
 
 if(window.innerWidth < 1200){
   addClassHeader()
@@ -11,6 +12,7 @@ if(window.innerWidth < 1200){
 }
 scrollHeader()
 callTabs()
+googleMap()
 showSubmenu(document.querySelectorAll('.submenu-questions'));
 
 
