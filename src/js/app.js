3e16bd7979203c3b9d95f39b4cc1784cd901b663
Kamarhinanton <Kamarhin_anton@ukr.net>
2022-12-02 12:13:11 +0200

    feat: add google map section + mob. - 2

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,6 @@ import {callMenu} from "./modules/addSubmenuClass.js";
 import {showSubmenu} from "./modules/showSubmenu.js";
 import {scrollHeader} from "./modules/scrollHeader.js";
 import {callTabs} from "./modules/callTabs.js";
-import {googleMap} from "./modules/googleMap.js";
 
 if(window.innerWidth < 1200){
   addClassHeader()
