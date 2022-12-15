8bf874fb7593102f72e2d3ce3aab379e16627ca9
Kamarhinanton <Kamarhin_anton@ukr.net>
2022-12-15 11:01:49 +0200

    feat: add scroll to top func

--- src/js/modules/filterItems.js
+++ src/js/modules/filterItems.js
@@ -4,18 +4,24 @@ export const filterItems = () => {
     elementsLink.forEach(link => link.addEventListener('change', function () {
       let attr = this.getAttribute('data-attr');
       let thisItems = document.querySelectorAll(`[data-group = ${attr}]`);
+      const obj = document.querySelector('.prebuilt__content');
+      const yOffset = -80;
       if (!this.checked) {
         thisItems.forEach(
           el => {
             el.classList.add('hide-item')
           }
         )
+        const y = obj.getBoundingClientRect().top + window.scrollY + yOffset;
+        window.scrollTo({top: y, behavior: 'smooth'});
       } else {
         thisItems.forEach(
           el => {
             el.classList.remove('hide-item')
           }
         )
+        const y = obj.getBoundingClientRect().top + window.scrollY + yOffset;
+        window.scrollTo({top: y, behavior: 'smooth'});
       }
     }))
   }
